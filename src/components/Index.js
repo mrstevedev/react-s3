b61aa0ed4d406c4f76e9f7f033ec2344a14f65bf
Steven Polido <steven.polido@vistage.com>
2020-06-09 18:11:57 -0700

    Added Index component

---
+++ src/components/Index.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+import UserForm from '../components/UserForm';
+
+class Index extends Component {
+    render() {
+        return (
+            <Fragment>
+                <UserForm />
+            </Fragment>
+        )
+    }
+}
+
+export default Index
