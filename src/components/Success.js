24f888246b4c2b2d228439764d019f813218c756
Steven Polido <steven.polido@vistage.com>
2020-06-09 18:14:41 -0700

    Added Success message

---
+++ src/components/Success.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+
+class Success extends Component {
+    render() {
+        return (
+            <Fragment>
+                Success
+            </Fragment>
+        )
+    }
+}
+
+export default Success
