29de57a16f3789e614057d2fc08cc3daba7b224d
Steven Polido <steven.polido@vistage.com>
2020-06-09 18:13:26 -0700

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import Container from '@material-ui/core/Container';
-import UserForm from './components/UserForm';
+import Index from './components/Index';
 import Header from './components/Header';
+import Success from './components/Success';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Header />
+      <Router>
+      <Header />
         <Container maxWidth="sm">
-          <UserForm />
+        <Route exact path="/" component={Index} />
+        <Route exact path="/submit" component={Success} />
         </Container>
+      </Router>
       </Fragment>
     );
   }
