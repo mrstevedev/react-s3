f5035e0e8363fb4d5d4601afb861df704646597f
Steven Polido <steven.polido@vistage.com>
2021-03-06 15:03:27 -0800

    Updated regex

--- src/util/validate.js
+++ src/util/validate.js
@@ -1,4 +1,4 @@
 export function validateEmail(email) {
-    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return regex.test(email);
 }
